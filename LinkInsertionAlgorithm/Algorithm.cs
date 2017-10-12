5ce656adaaa31e5f44edd5528c3f553a1707c1c1
e.manushkin <e.manushkin@gmail.com>
2017-10-13 02:06:16 +0300

    half programm

---
+++ LinkInsertionAlgorithm/Algorithm.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LinkInsertionAlgorithm
+{
+    public static class Algorithm
+    {
+        public static string InsertLinks(string text, IEnumerable<string> links)
+        {
+            var document = new HtmlDocument();
+
+            document.LoadHtml(text);
+
+            var paragraphNodes = document.DocumentNode.SelectNodes($"//p")?.ToList() ?? null;
+
+            string pattern = @"(\s*<a .+?>.+?</a>\.?|[^<>]+?\.)|(\s*<a .+?>.+?</a>\??|[^<>]+?\?)|(\s*<a .+?>.+?</a>!?|[^<>]+?!)";
+
+            foreach (var item in paragraphNodes)
+            {
+                string[] sentences = Regex.Split(item.InnerHtml, pattern).Where(s => !string.IsNullOrEmpty(s)).ToArray(); ;
+                foreach (string s in sentences)
+                {
+                    Console.WriteLine("{0}", s);
+                }
+                Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            }
+            
+
+            //paragraphNodes[0].InnerHtml = "test";
+
+
+
+            //if (nodes != null && nodes.Count > 0)
+            //{
+            //    return nodes.Select((x, i) => $"<a href='#article-text__subtitle--{i + 1}' class='article-header__anchor-link'>{x.InnerText}</a>").ToArray();
+            //}
+            //return new string[] { };
+            string result = document.DocumentNode.InnerHtml;
+
+            return result;
+        }
+    }
+}
--- LinkInsertionAlgorithm/Program.cs
+++ LinkInsertionAlgorithm/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LinkInsertionAlgorithm
@@ -10,6 +11,50 @@ namespace LinkInsertionAlgorithm
     {
         static void Main(string[] args)
         {
+            string text = @"<img src='test.jpg'>
+                <p>Это первый абзац текста. Он состоит из трех предложений. Это первое предложение. Это второе предложение? Это третье предложение!</p>
+                <h2>Подзаголовок</h2>
+                <p>А это второй абзац текста, он в отличие от первого состоит из двух предложений. Это первое. <a href='http://test0.com'>Эта ссылка тут изначально. Она состоит из двух предложений</a>. А это второе.</p>
+                <p>Это третий абзац текста. Он состоит из трех предложений. Это первое предложение. Это второе предложение? Это третье предложение!</p>";
+
+            //text = Regex.Replace(text, @"\s+", " ");
+
+            Console.Write("Количество ссылок для вставки в базовый текст: ");
+
+            int count = 0;
+
+            int.TryParse(Console.ReadLine(), out count);
+
+            List<string> links = GenerateLinks(count);
+
+            string resultText = Algorithm.InsertLinks(text, links);
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Базовый текст:");
+            Console.WriteLine(text);
+
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Текст после вставки ссылок:");
+            Console.WriteLine(resultText);
+
+            Console.ReadKey();
+        }
+
+        public static List<string> GenerateLinks(int count)
+        {
+            const string linkTemplate1 = "<a href='http://test{0}.com'>Это ссылка #{0}.</a>.";
+            const string linkTemplate2 = "<a href='http://test{0}.com'>Это ссылка #{0}. Она состоит из нескольких предложений</a>.";
+
+            var result = new List<string>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(rnd.Next(0, 2) == 0 ? string.Format(linkTemplate1, i + 1) : string.Format(linkTemplate2, i + 1));
+            }
+
+            return result;
         }
     }
 }
